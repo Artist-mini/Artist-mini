d36b866d346cf46f4f580edf7a01fcf232f865b7
rmfls0606 <rmfls0606@naver.com>
2023-03-18 20:44:56 +0900

    develop commit

---
+++ Artist-mini/App/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Artist-mini
+//
+//  Created by 이상민 on 2023/03/18.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
