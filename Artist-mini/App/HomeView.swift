c4170e1ea3e37f74954a10652b668fc6aa6b682f
rmfls0606 <rmfls0606@naver.com>
2023-03-18 20:47:44 +0900

    feature-TabView

--- Artist-mini/App/HomeView.swift
+++ Artist-mini/App/HomeView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct HomeView: View {
+    //propert
+    @State private var selection = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        /**첫번째 tabItem이 먼저 보여지는데 다른 페이지가 먼저 보여지게 할려면
+         보이게 하고 싶은 tabItem에 .tag를 붙이고 TabView에 selection 프로퍼티에
+         타입명이 binding이기에 @state로 원하는 페이지 tag값을 넣어준 후 tabview에 binding하여 넣어주면
+         원하는 페이지가 처음으로 나오게 된다.
+        ***/
+        TabView(selection: $selection){
+            Text("검색")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("검색")
+                }
+            ContentView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("홈")
+                }.tag(1)
+            Text("갤러리")
+                .tabItem {
+                    Image(systemName: "photo.fill")
+                    Text("갤러리")
+                }
+        }
     }
 }
 
