45e573727d2dd37f2983956c0629855d23c87bfd
rmfls0606 <rmfls0606@naver.com>
2023-03-18 20:39:32 +0900

    Initial Commit

---
+++ Artist-mini/Artist_miniApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artist_miniApp.swift
+//  Artist-mini
+//
+//  Created by 이상민 on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct Artist_miniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
