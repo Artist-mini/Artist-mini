105626d4a4796d203fd55423c47975d9efd40fd8
psm3859 <psm3859@naver.com>
2023-03-31 10:19:49 +0900

    TextSearchView push

--- Artist-mini/View/TextSearchView/TextSearchView.swift
+++ Artist-mini/View/TextSearchView/TextSearchView.swift
@@ -9,38 +9,39 @@ import SwiftUI
 
 struct TextSearchView: View {
     @State var searchText: String = " "
-       var body: some View {
-           HStack{
-               //검색 아이콘
-           Image(systemName: "magnifyingglass")
-                   .frame(width: .infinity, height: 10)
-               .padding()
-               
-               // 입력할 텍스트 창
-               TextField("검색하고 싶은거?", text: $searchText)
-                   .padding()
-               
-               //확인버튼
-           Button {
-               saveText()
-           } label: {
-               Image(systemName: "checkmark.circle")
-                   .frame(width: .infinity, height: 10)
-                   
-                   .padding()
-           }
-       }
-           .background(
-                   Capsule(style: .circular)
-                   .stroke(Color.blue)
-                   .frame(width: 350, height: 50))
-           .padding()
-       }
+        var body: some View {
+            HStack{
+                //검색 아이콘
+            Image(systemName: "magnifyingglass")
+                    .frame(width: .infinity, height: 10)
+                .padding()
+                
+                // 입력할 텍스트 창
+                TextField("검색하고 싶은거", text: $searchText)
+                    .padding()
+                
+                //확인버튼
+            Button {
+                saveText()
+            } label: {
+                Image(systemName: "checkmark.circle")
+                    .frame(width: .infinity, height: 10)
+                    
+                    .padding()
+            }
+        }
+            .background(
+                    Capsule(style: .circular)
+                    .stroke(Color.blue)
+                    .frame(width: 350, height: 50))
+            .padding()
+        }
 
-       func saveText(){
-         
-           searchText = " "
-       }
+        func saveText(){
+          
+            searchText = " "
+        }
+
 }
 
 struct TextSearchView_Previews: PreviewProvider {
